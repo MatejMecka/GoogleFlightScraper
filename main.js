c6bd05e875e0e59d0d02504a247d7e3cc5527f5f
Matej Plavevski <matej.plavevski+github@gmail.com>
2017-10-23 15:49:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-'use strict';
-var request = require('request');
-var cheerio = require('cheerio');
-
+'use strict'
+var request = require('request')
+var cheerio = require('cheerio')
 
 /**
  * Gives Flight information
@@ -9,64 +8,61 @@ var cheerio = require('cheerio');
  * @param {function} callback
  */
 
+function FlightTracking(flight, callback){
+	request('http://www.google.com/search?q=' + flight, function(err,  resp, html) {
+		if (!err && resp.statusCode == 200){
+			var $ = cheerio.load(html)
+			var flight = $('h3', '#res').eq(0).text()
+			var status = $('._lMf').text()
+			var table = $('.g  table td')
+			var depcountry = table.eq(8).text()
+			var depcode = table.eq(3).text()
+			var deptime = table.eq(4).text() + ' ' + table.eq(5).text()
+			var depterminal = table.eq(6).text() + ' ' + table.eq(11).text()
+			var arrcountry = table.eq(19).text()
+			var arrcode = table.eq(14).text()
+			var arrtime = table.eq(15).text() + ' ' + table.eq(16).text()
+			var arrterminal = table.eq(17).text() + ' ' + table.eq(22).text()
+			var info = table.eq(1).text()
 
- function FlightTracking(flight, callback){
- 	request("http://www.google.com/search?q=" + flight, function(err,  resp, html) {
-        if (!err && resp.statusCode == 200){
-        	var $ = cheerio.load(html);
-        	var flight = $('h3', '#res').eq(0).text();
-        	var status = $('._lMf').text();
-        	var table = $('.g  table td');
-        	var depcountry = table.eq(8).text();
-                var depcode = table.eq(3).text();
-        	var deptime = table.eq(4).text() + " " + table.eq(5).text();
-        	var depterminal = table.eq(6).text() + " " + table.eq(11).text();
-        	var arrcountry = table.eq(19).text();
-                var arrcode = table.eq(14).text();
-        	var arrtime = table.eq(15).text() + " " + table.eq(16).text();
-        	var arrterminal = table.eq(17).text() + " " + table.eq(22).text();
-        	var info = table.eq(1).text();
-
-		if(arrcountry == ''){
-						
-			throw new Error("The submitted Flight  was not found or doesn't exist!")
+			if(arrcountry == ''){
+				throw new Error('The submitted Flight  was not found or doesn\'t exist!')
+			}
 		
+			else {
+
+				var flightinfo = {
+					Flight: flight, 
+					Status: status, 
+					DepartureCountry: depcountry, 
+					DepartureCode: depcode,
+					DepartureTime: deptime, 
+					DepartureTerminal: depterminal, 
+					ArrivalCountry: arrcountry,
+					ArrivalCode: arrcode,
+					ArrivalTime: arrtime,
+					ArrivalTerminal:arrterminal,
+					Information:info
+				}
+
+				callback(flightinfo)
+
+			}	
+
 		}
-		
-                else {
 
-                	var flightinfo = {
-                		Flight: flight, 
-                		Status: status, 
-                		DepartureCountry: depcountry, 
-                                DepartureCode: depcode,
-                		DepartureTime: deptime, 
-                		DepartureTerminal: depterminal, 
-                		ArrivalCountry: arrcountry,
-                                ArrivalCode: arrcode,
-                		ArrivalTime: arrtime,
-                		ArrivalTerminal:arrterminal,
-                		Information:info
-                	}
-
-                	callback(flightinfo)
-
-	       }	
-
-        }
-
-        else {	
+		else {	
 
 
-		throw new Error("There was a problem retriving the flight information. Probably caused by a captcha or there isn't an active internet connnection. Error Message: " + err.message)		
+			throw new Error('There was a problem retriving the flight information. Probably caused by a captcha or there isn\'t an active internet connnection. Error Message: ' + err.message)		
 
 
-                }	
+		}	
 
 
-	 });
+	})
 
 }
 
- module.exports = FlightTracking;
+module.exports = FlightTracking
 
