7cafa5a0118f642d201ec929bff9af97566bb383
matejmecka <matej.plavevski+github@gmail.com>
2018-07-07 17:06:35 +0200

    More Tests

--- test/test.js
+++ test/test.js
@@ -4,9 +4,17 @@ var tracker = require('../main.js')
 
 it('Check if flight works', function(done) {
   tracker("JU 500", function(flightinfo,error){
-    
+
     var status = flightinfo["Status"]
+    var country = flightinfo["DepartureCountry"]
+    var countryCode = flightinfo["DepartureCode"]
+    var arrivalCountry = flightinfo["ArrivalCountry"]
+    var arrivalCode = flightinfo["ArrivalCode"]
     expect(status).to.equal('Scheduled');
+    expect(countryCode).to.equal('BEG')
+    expect(country).to.equal('Belgrade')
+    expect(arrivalCountry).to.equal('New York City')
+    expect(arrivalCode).to.equal('JFK')    
     done();
         
    });
